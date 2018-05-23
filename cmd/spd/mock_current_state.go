eda1e4b127c0d376d7560dc82161ac682053b261
Yesika Ramirez <yemramirezca@gmail.com>
2018-05-23 20:54:15 +0200

    Add mock to simulate current state

---
+++ cmd/spd/mock_current_state.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/yemramirezca/SPDT/internal/types"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+)
+
+
+func CurrentState() types.State {
+	var state types.State
+
+	data, err := ioutil.ReadFile("cmd/spd/mock_current_state.json")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	err = json.Unmarshal(data, &state)
+	return state
+}
