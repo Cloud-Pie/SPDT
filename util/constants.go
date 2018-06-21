34807804cd1bbd14b74397f8137448981dfb46e7
Yesika Ramirez <yemramirezca@gmail.com>
2018-06-21 10:23:12 +0200

    clean up not used links

--- util/constants.go
+++ util/constants.go
@@ -1,9 +1,6 @@
 package util
 
-const URL_SCHEDULER = "http://localhost:8080/api/states"
-const URL_PROFILER = "http://localhost:8082/api/profiles"
-const URL_FORECAST = "http://localhost:8081/api/forecast"
-const URL_SERIE_PROCESSING = "http://localhost:5003/api/peaks"
+const URL_SERIE_PROCESSING = "http://localhost:5003/api/peaks/plot"
 const PRICES_FILE = "prices_test.yml"
 const TIME_LAYOUT = "02-01-2006, 15:04:05 MST"
 const CONFIG_FILE = "config.yml"
