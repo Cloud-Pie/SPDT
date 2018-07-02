687933b8557b5f82db8bcafeac277056458e69ad
Yesika Ramirez <yemramirezca@gmail.com>
2018-07-02 20:54:12 +0200

    Add functions to rest clients to retrieve VMs and the current state

--- rest_clients/performance_profiles/client.go
+++ rest_clients/performance_profiles/client.go
@@ -8,8 +8,8 @@ import (
 )
 
 
-func GetPerformanceProfiles(endpoint string) (types.PerformanceProfile, error){
-	performanceProfile := types.PerformanceProfile{}
+func GetPerformanceProfiles(endpoint string) (types.ServiceProfile, error){
+	performanceProfile := types.ServiceProfile{}
 	response, err := http.Get(endpoint)
 	if err != nil {
 		return performanceProfile,err
@@ -25,4 +25,24 @@ func GetPerformanceProfiles(endpoint string) (types.PerformanceProfile, error){
 		return performanceProfile,err
 	}
 	return performanceProfile,err
+}
+
+
+func GetVMsProfiles(endpoint string) ([]types.VmProfile, error){
+	vmList := []types.VmProfile{}
+	response, err := http.Get(endpoint)
+	if err != nil {
+		return vmList,err
+	}
+	defer response.Body.Close()
+	data, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return vmList,err
+	}
+	err = json.Unmarshal(data, &vmList)
+	if err != nil {
+		return vmList,err
+	}
+	return vmList,err
 }
--- rest_clients/scheduler/client.go
+++ rest_clients/scheduler/client.go
@@ -5,10 +5,34 @@ import (
 	"encoding/json"
 	"net/http"
 	"bytes"
+	"io/ioutil"
 )
 
-func CreateState(state types.State, endpoint string) error{
+
+
+func CreateState(state types.State, endpoint string) error {
 	jsonValue, _ := json.Marshal(state)
 	_, err := http.Post(endpoint, "application/json", bytes.NewBuffer(jsonValue))
 	return err
 }
+
+
+func CurrentState(endpoint string) (types.State, error) {
+	currentState := types.State{}
+	response, err := http.Get(endpoint)
+	if err != nil {
+		return currentState, err
+	}
+
+	defer response.Body.Close()
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return  currentState, err
+	}
+	err = json.Unmarshal(data, &currentState)
+	if err != nil {
+		return  currentState, err
+	}
+	return  currentState, err
+}
+
