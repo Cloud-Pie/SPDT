95377c86d2ff94a5d5d79cb2fb87ce9be1a7d0d5
Yesika Ramirez <yemramirezca@gmail.com>
2018-07-07 12:47:48 +0200

    Added endpoints to get information stored in db

--- cmd/spd/server.go
+++ cmd/spd/server.go
@@ -2,13 +2,48 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	db "github.com/Cloud-Pie/SPDT/storage/policies"
+	"net/http"
+	"github.com/Cloud-Pie/SPDT/util"
+	"log"
 )
 
 func SetUpServer() *gin.Engine{
 	router := gin.Default()
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.GET("/api/forecast", serverCall)
+	router.GET("/api/policy", policy)
+	router.GET("/api/policies", policies)
 	return router
 }
 
+func policy(c *gin.Context) {
+	id := c.Param("id")
+
+	policyDAO := db.PolicyDAO{
+		util.DEFAULT_DB_SERVER_POLICIES,
+		util.DEFAULT_DB_POLICIES,
+	}
+	policyDAO.Connect()
+	policy,err := policyDAO.FindByID(id)
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	c.JSON(http.StatusOK, policy)
+}
+
+func policies(c *gin.Context) {
+	policyDAO := db.PolicyDAO{
+		util.DEFAULT_DB_SERVER_POLICIES,
+		util.DEFAULT_DB_POLICIES,
+	}
+	policyDAO.Connect()
+	policies,err := policyDAO.FindAll()
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	c.JSON(http.StatusOK, policies)
+}
 
