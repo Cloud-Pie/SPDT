b9f27e444ce7fac84652a38913a72100343b1cc9
Yesika Ramirez <yemramirezca@gmail.com>
2018-10-27 15:56:24 +0200

    granularity

--- util/config.go
+++ util/config.go
@@ -15,12 +15,17 @@ type Component struct {
 	ApiKey string	`yaml:"api-key"`
 }
 
+//Struct that models the external components to which SPDT should be connected
+type ForecastComponent struct {
+	Endpoint string	`yaml:"endpoint"`
+	Granularity string	`yaml:"granularity"`
+}
+
 //The future timespan for which the autoscaling policy is derived
 type ScalingHorizon struct {
 	StartTime time.Time	`yaml:"start-time"`
 	EndTime	time.Time	`yaml:"end-time"`
 }
-
 
 //
 type PricingModel struct {
@@ -42,19 +47,20 @@ type PolicySettings struct{
 
 //Struct that models the system configuration to derive the scaling policies
 type SystemConfiguration struct {
-	CSP                          string         `yaml:"CSP"`
-	Region                       string         `yaml:"region"`
-	AppName                      string         `yaml:"app-name"`
-	MainServiceName              string         `yaml:"main-service-name"`
-	AppType                      string         `yaml:"app-type"`
-	PricingModel                 PricingModel   `yaml:"pricing-model"`
-	ForecastingComponent         Component      `yaml:"forecasting-component"`
-	PerformanceProfilesComponent Component      `yaml:"performance-profiles-component"`
-	SchedulerComponent           Component      `yaml:"scheduler-component"`
-	ScalingHorizon               ScalingHorizon `yaml:"scaling-horizon"`
-	PreferredAlgorithm           string         `yaml:"preferred-algorithm"`
-	PolicySettings               PolicySettings `yaml:"policy-settings"`
-	PullingInterval        		 int   `yaml:"pulling-interval"`
+	Host 						 string			   `yaml:"host"`
+	CSP                          string            `yaml:"CSP"`
+	Region                       string            `yaml:"region"`
+	AppName                      string            `yaml:"app-name"`
+	MainServiceName              string            `yaml:"main-service-name"`
+	AppType                      string            `yaml:"app-type"`
+	PricingModel                 PricingModel      `yaml:"pricing-model"`
+	ForecastComponent            ForecastComponent `yaml:"forecasting-component"`
+	PerformanceProfilesComponent Component         `yaml:"performance-profiles-component"`
+	SchedulerComponent           Component         `yaml:"scheduler-component"`
+	ScalingHorizon               ScalingHorizon    `yaml:"scaling-horizon"`
+	PreferredAlgorithm           string            `yaml:"preferred-algorithm"`
+	PolicySettings               PolicySettings    `yaml:"policy-settings"`
+	PullingInterval              int               `yaml:"pulling-interval"`
 }
 
 //Method that parses the configuration file into a struct type
